ad8f2a3ec7d0ad46933817a04be409aa1094b3fb
kelianor <you@example.com>
2026-01-12 10:51:30 +0300

    Complete task 1, task 2

--- Lab15/1.cpp
+++ Lab15/1.cpp
@@ -1,178 +1,335 @@
 #include <iostream>
 #include <cstring>
-#include <time.h>
 using namespace std;
 
-struct emp
+enum Fields {FirstName, LastName, Title, Year, Birth, Salary, Phone};
+
+struct Date
 {
-    int empNo;
-    char name[80];
-    double salary;
+    int year;
+    int month;
+    int day;
 };
 
-void inputEmp(emp&);
-void swap(emp&, emp&);
-int minSalaryFrom(emp*, int, int);
-int minNameFrom(emp*, int, int);
+struct Emp
+{
+    char firstName[80];
+    char lastName[80];
+    char title[80];
+    int year;
+    Date birth;
+    double salary;
+    long long phone;
+};
 
-void shellSortName(emp*, int);
-void selSortName(emp*, int);
+int cmp(double &a, double &b);
+int cmp(int &a, int &b);
+int cmp(long long &a, long long &b);
 
-void shellSortSalary(emp*, int);
-void selSortSalary(emp*, int);
+int compare(double&, double&, Fields, bool);
+int compare(Emp&, Emp&, Fields, bool);
+
+void swap(Emp&, Emp&);
+int minFrom(Emp*, int, int, Fields, bool);
+
+void selSort(Emp*, int, Fields, bool);
+void shellSort(Emp*, int, Fields, bool);
+
+void inputEmp(Emp&);
+void printEmp(Emp&);
+
+int regularSearch(Emp*, int, Fields, Emp&);
+int binarySearch(Emp*, int, Fields, Emp&);
 
 int main()
 {
-    int n;
+    int choice;
     do
     {
-        cout << "Введите размер массива: ";
-        cin >> n;
-    } 
-    while (n <= 0);
+        cout << "Выберите задание: 1 - первое, 2 - второе, 3 - третье, 0 - завершить работу: ";
+        cin >> choice;
+
+        int n;
+        do
+        {
+            cout << "Введите размер массива: ";
+            cin >> n;
+        } 
+        while (n <= 0);
     
-    emp *arr = new emp[n];
+        Emp *arr = new Emp[n];
 
-    if (!arr)
-    {
-        cout << "Не удалось создать массив!\n";
-        system("pause");
-        return 0;
-    }
+        if (!arr)
+        {
+            return 0;
+        }
 
-    for (int i = 0; i < n; i++)
-    {
-        cout << "Заполнение " << i << "-й ячейки:\n";
-        inputEmp(arr[i]);
-        cout << endl;
-    }
+        for (int i = 0; i < n; i++)
+        {
+            cout << "Заполнение " << i << "-й ячейки:\n";
+            inputEmp(arr[i]);
+            cout << endl;
+        }
 
-    // Сортировка
+        Emp *sortArr = new Emp[n];
+        bool sorted = false;
 
-    emp *sortArr = new emp[n];
+        if (!sortArr)
+        {
+            return 0;
+        }
 
-    if (!sortArr)
-    {
-        cout << "Не удалось создать массив!\n";
-        system("pause");
-        return 0;
-    }
+        switch(choice)
+        {
+            case 0:
+            {
+                delete[] sortArr;
+                delete[] arr;
+                break;
+            }
+            case 1:
+            {
+                cout << "\nСортировка методом выбора поля фамилии сотрудника:\n";
 
-    cout << "\nСортировка методом выбора поля фамилии сотрудника:\n";
-    
-    for (int i = 0; i < n; i++)
-    {
-        sortArr[i] = arr[i];
-    }
-    selSortName(sortArr, n);
+                for (int i = 0; i < n; i++)
+                {
+                    sortArr[i] = arr[i];
+                }
+                selSort(sortArr, n, Fields::LastName, false);
+                for(int i = 0; i < n; i++) printEmp(sortArr[i]);
 
-    for (int i = 0; i < n; i++)
-    {
-        cout << sortArr[i].empNo << " - " << sortArr[i].name << " - " << sortArr[i].salary << "$\n";
-    }
+                cout << "\nСортировка методом Шелла поля фамилии сотрудника:\n";
+                for (int i = 0; i < n; i++)
+                {
+                    sortArr[i] = arr[i];
+                }
+                shellSort(sortArr, n, Fields::LastName, false);
+                for(int i = 0; i < n; i++) printEmp(sortArr[i]);
 
-    cout << "\nСортировка методом Шелла поля фамилии сотрудника:\n";
-    for (int i = 0; i < n; i++)
-    {
-        sortArr[i] = arr[i];
-    }
-    shellSortName(sortArr, n);
+                cout << "\nСортировка методом выбора поля зарплаты сотрудника:\n";
+                for (int i = 0; i < n; i++)
+                {
+                    sortArr[i] = arr[i];
+                }
+                selSort(sortArr, n, Fields::Salary, true);
+                for(int i = 0; i < n; i++) printEmp(sortArr[i]);
 
-    for (int i = 0; i < n; i++)
-    {
-        cout << sortArr[i].empNo << " - " << sortArr[i].name << " - " << sortArr[i].salary << "$\n";
-    }
+                cout << "\nСортировка методом Шелла поля зарплаты сотрудника:\n";
+                for (int i = 0; i < n; i++)
+                {
+                    sortArr[i] = arr[i];
+                }
+                shellSort(sortArr, n, Fields::Salary, true);
+                for(int i = 0; i < n; i++) printEmp(sortArr[i]);
 
-    cout << "\nСортировка методом выбора поля зарплаты сотрудника:\n";
-    for (int i = 0; i < n; i++)
-    {
-        sortArr[i] = arr[i];
-    }
-    selSortSalary(sortArr, n);
+                sorted = true;
+                break;
+            }
+            case 2:
+            {
+                int chosen;
+                cout << "Выберите поле: 0 - FirstName, 1 - LastName, 2 - Title, 3 - Year, 4 - Birth, 5 - Salary, 6 - Phone: ";
+                cin >> chosen;
+                Fields chosenField = static_cast<Fields>(chosen);
 
-    for (int i = 0; i < n; i++)
-    {
-        cout << sortArr[i].empNo << " - " << sortArr[i].name << " - " << sortArr[i].salary << "$\n";
-    }
+                Emp key = {};
+        
 
-    cout << "\nСортировка методом Шелла поля зарплаты сотрудника:\n";
-    for (int i = 0; i < n; i++)
-    {
-        sortArr[i] = arr[i];
-    }
-    shellSortSalary(sortArr, n);
 
-    for (int i = 0; i < n; i++)
-    {
-        cout << sortArr[i].empNo << " - " << sortArr[i].name << " - " << sortArr[i].salary << "$\n";
-    }
-    
-    delete[] arr;
-    delete[] sortArr;
+                switch(chosenField)
+                {
+                    case FirstName:
+                    {
+                        cout << "Введите имя для поиска: ";
+                        cin.ignore();
+                        cin.getline(key.firstName, 80);
+                        break;
+                    }
+                    case LastName:
+                    {
+                        cout << "Введите фамилию для поиска: ";
+                        cin.ignore();
+                        cin.getline(key.lastName, 80);
+                        break;
+                    }
+                    case Title:
+                    {
+                        cout << "Введите должность для поиска: ";
+                        cin.ignore();
+                        cin.getline(key.title, 80);
+                        break;
+                    }
+                    case Year:
+                    {
+                        cout << "Введите год начала работы для поиска: ";
+                        cin >> key.year;
+                        break;
+                    }
+                    case Birth:
+                    {
+                        cout << "Введите год рождения для поиска: ";
+                        cin >> key.birth.year;
+                        cout << "Введите месяц рождения для поиска: ";
+                        cin >> key.birth.month;
+                        cout << "Введите день рождения для поиска: ";
+                        cin >> key.birth.day;
+                        break;
+                    }
+                    case Salary:
+                    {
+                        cout << "Введите оклад для поиска: ";
+                        cin >> key.salary;
+                        break;
+                    }
+                    case Phone:
+                    {
+                        cout << "Введите номер телефона для поиска: +";
+                        cin >> key.phone;
+                        break;
+                    }
+                }
+
+                if(sorted)
+                {
+                    int idx = binarySearch(sortArr, n, chosenField, key);
+                    if (idx != -1)
+                    {
+                        cout << "Элемент найден под индексом " << idx << endl;
+                        printEmp(sortArr[idx]);
+                    }
+                    else
+                    {
+                        cout << "Элемент не найден" << endl;
+                    }
+                }
+                else
+                {
+                    int idx = regularSearch(arr, n, chosenField, key);
+                    if (idx != -1)
+                    {
+                        cout << "Элемент найден под индексом " << idx << endl;
+                        printEmp(arr[idx]);
+                    }
+                    else
+                    {
+                        cout << "Элемент не найден" << endl;
+                    }
+                }
+                break;
+            }
+        }
+    } while (choice != 0);
     return 0;
 }
 
-void inputEmp(emp &emp)
+void inputEmp(Emp &emp)
 {
-    cout << "Введите какой это сотрудник по счёту: ";
-    cin >> emp.empNo;
+    cout << "Введите фамилию сотрудника: ";
+    cin.get();
+    cin.get(emp.lastName, 80);
     cout << "Введите имя сотрудника: ";
     cin.get();
-    cin.get(emp.name, 80);
+    cin.get(emp.firstName, 80);
+    cout << "Введите должность сотрудника: ";
+    cin.get();
+    cin.get(emp.title, 80);
     cout << "Введите заработную плату сотрудника: ";
     cin >> emp.salary;
+    cout << "Введите год с которого он начал работать: ";
+    cin >> emp.year;
+    cout << "Введите номер телефона: +";
+    cin >> emp.phone;
+    cout << "Введите год рождения: ";
+    cin >> emp.birth.year;
+    cout << "Введите месяц рождения: ";
+    cin >> emp.birth.month;
+    cout << "Введите день рождения: ";
+    cin >> emp.birth.day;
 }
 
-void swap(emp &first, emp &second)
+int compare(Emp &a, Emp &b, Fields field, bool reverse)
 {
-    emp tmp = first;
-    first = second;
-    second = tmp;
-}
-
-int minSalaryFrom(emp *m, int position, int length)
-{
-    int minIndex = position;
-    for(int i = position + 1; i < length; i++)
+    int state = 0;
+    switch(field)
     {
-        if(m[i].salary > m[minIndex].salary)
+        case FirstName:
         {
-            minIndex = i;
+            state = strcmp(a.firstName, b.firstName);
+            break;
+        }
+        case LastName:
+        {
+            state = strcmp(a.lastName, b.lastName);
+            break;
+        }
+        case Title:
+        {
+            state = strcmp(a.title, b.title);
+            break;
+        }
+        case Year:
+        {
+            state = cmp(a.year, b.year);
+            break;
+        }
+        case Birth:
+        {
+            if(a.birth.year != b.birth.year)
+            {
+                state = cmp(a.birth.year, b.birth.year);
+            }
+            else if(a.birth.month != b.birth.month)
+            {
+                state = cmp(a.birth.month, b.birth.month);
+            }
+            else
+            {
+                state = cmp(a.birth.day, b.birth.day);
+            }
+
+            break;
+        }
+        case Salary:
+        {
+            state = cmp(a.salary, b.salary);
+            break;
+        }
+        case Phone:
+        {
+            state = cmp(a.phone, b.phone);
+            break;
         }
     }
-    return minIndex;
-}
 
-int minNameFrom(emp *m, int position, int length)
-{
-    int minIndex = position;
-    for(int i = position + 1; i < length; i++)
+    if(reverse)
     {
-        if(strcmp(m[i].name, m[minIndex].name) < 0)
-        {
-            minIndex = i;
-        }
+        state = -state;
     }
-    return minIndex;
+
+    return state;
 }
 
-void selSortSalary(emp *m, int length)
+int cmp(int &a, int &b)
 {
-    for(int i = 0; i < length - 1; i++)
-    {
-        swap(m[i], m[minSalaryFrom(m, i, length)]);
-    }
+    return (a > b) ? 1 : ((a == b) ? 0 : -1);
 }
 
-void selSortName(emp *m, int length)
+int cmp(double &a, double &b)
 {
-    for(int i = 0; i < length - 1; i++)
-    {
-        swap(m[i], m[minNameFrom(m, i, length)]);
-    }
+    return (a > b) ? 1 : ((a == b) ? 0 : -1);
 }
 
-void shellSortSalary(emp *m, int n)
+int cmp(long long &a, long long &b)
+{
+    return (a > b) ? 1 : ((a == b) ? 0 : -1);
+}
+
+void printEmp(Emp &emp)
+{
+    cout << emp.firstName << " " << emp.lastName << ", родившийся " << emp.birth.year << '.' << emp.birth.month << '.' << emp.birth.day << " работает " << emp.title << "ем с " << emp.year << " года с зарплатой " << emp.salary << " тел. +" << emp.phone << endl;
+}
+
+void shellSort(Emp *m, int n, Fields field, bool reverse)
 {
     for(int i = n / 2; i > 0; i = i / 2)
     {
@@ -180,11 +337,9 @@ void shellSortSalary(emp *m, int n)
         {
             for(int k = j; k > -1; k = k - i)
             {
-                if(m[k].salary < m[k + i].salary)
+                if(compare(m[k], m[k + i], field, reverse) > 0)
                 {
-                    emp t = m[k];
-                    m[k] = m[k + i];
-                    m[k + i] = t;
+                    swap(m[k], m[k + i]);
                 }
                 else
                 {
@@ -195,25 +350,66 @@ void shellSortSalary(emp *m, int n)
     }
 }
 
-void shellSortName(emp *m, int n)
+void selSort(Emp *m, int length, Fields field, bool reverse)
 {
-    for(int i = n / 2; i > 0; i = i / 2)
+    for(int i = 0; i < length - 1; i++)
     {
-        for(int j = 0; j < n - i; j++)
+        swap(m[i], m[minFrom(m, i, length, field, !reverse)]);
+    }
+}
+
+void swap(Emp &a, Emp &b)
+{
+    Emp t = a;
+    a = b;
+    b = t;
+}
+
+int minFrom(Emp* m, int position, int length, Fields field, bool reverse)
+{
+    int minIndex = position;
+    for(int i = position + 1; i < length; i++)
+    {
+        if(compare(m[i], m[minIndex], field, reverse) > 0)
         {
-            for(int k = j; k > -1; k = k - i)
-            {
-                if(strcmp(m[k].name, m[k + i].name) > 0)
-                {
-                    emp t = m[k];
-                    m[k] = m[k + i];
-                    m[k + i] = t;
-                }
-                else
-                {
-                    k = 0;
-                }
-            }
+            minIndex = i;
         }
     }
+    return minIndex;
+}
+
+int regularSearch(Emp* m, int n, Fields field, Emp& key)
+{
+  for (int i = 0; i < n; i++)
+  {
+    if (compare(m[i], key, field, false) == 0)
+    {
+      return i;
+    }
+  }
+  return -1;
+}
+
+int binarySearch(Emp* m, int n, Fields field, Emp& key)
+{
+  int l = 0;
+  int r = n - 1;
+  while (l <= r)
+  {
+    int mid = l + (r - l) / 2;
+    int c = compare(m[mid], key, field, false);
+    if (c == 0)
+    {
+      return mid;
+    }
+    else if (c < 0)
+    {
+      l = mid + 1;
+    }
+    else
+    {
+      r = mid - 1;
+    }
+  }
+  return -1;
 }
