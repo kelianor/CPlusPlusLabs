8fb54c5b70cac3514dd3795081f7dea3bb198cf4
kelianor <86879455+kelianor@users.noreply.github.com>
2026-03-07 15:36:32 +0300

    Finished lab 18

--- Lab18/4.cpp
+++ Lab18/4.cpp
@@ -46,7 +46,7 @@ class Airport
 
     int* appendLastFlight(int code)
     {
-        for(int i = 1; i < MAX_LAST_FLIGHTS; i++)
+        for(int i = MAX_LAST_FLIGHTS-1; i > 0; i--)
         {
             this->lastFlightCodes[i] = this->lastFlightCodes[i-1];
         }
@@ -58,7 +58,7 @@ class Airport
 
     char* setAirportCode(char *code)
     {
-        regex airCode(R"([a-z]{3,3})");
+        regex airCode(R"([a-zA-Z]{3,3})");
         if(regex_match(code, airCode))
         {
             strcpy(this->airportCode, code);
@@ -111,9 +111,8 @@ class Airport
         cout << "Проработал конструтор с параметрами\n";
     }
 
-    Airport()
+    Airport() : Airport(0, 0.0, 'A', true, "NON")
     {
-        Airport(0, 0.0, 'A', true, "MSK");
         cout << "С помощью конструктора без параметров\n";
     }
 
@@ -141,13 +140,101 @@ class Airport
         cout << endl;
     }
 
-    double СвангаватьКоличествоПолётов()
+    double СвангаватьКоличествоПолётов(int времяВылета, int  времяПрилета, int текущееКоличествоСамолетов, int промежутокВремени)
     {
-
+        if(времяПрилета <= 0 | времяВылета <= 0)
+        {
+            return -1;
+        }
+        double СамолетовБудет = текущееКоличествоСамолетов + промежутокВремени / времяПрилета - промежутокВремени / времяВылета;
+        return (СамолетовБудет < 0 ? 0 : СамолетовБудет);
     }
 };
 
 int main()
 {
+    Airport MSK;
+    MSK.Print();
+    cout << "Заполнение\n";
+    MSK.setVisitsPerDay(1563);
+    MSK.setAvgNumOfFlightsPerDay(250);
+    MSK.setAirClass('B');
+    MSK.setInUseFlag(true);
+    MSK.setAirportCode("MSK");
+    for(int i = 0; i < MAX_LAST_FLIGHTS; i++)
+    {
+        int airCode;
+        cout << "Код полёта (под индексом " << i << "): ";
+        cin >> airCode;
+        MSK.appendLastFlight(airCode);
+    }
+    MSK.Print();
+    
+    Airport SVO(5, 7.5, 'F', false, "SVO");
+    SVO.appendLastFlight(1200);
+    SVO.appendLastFlight(1213);
+    SVO.appendLastFlight(5469);
+    SVO.appendLastFlight(5695);
+    SVO.Print();
+    
+    
+    double avgNumOfFlightsPerDay;
+    char airClass; 
+    bool inUse; 
+    char airportCode[SIZE];
+    
+    Airport *DME = new Airport;
+    
+    int v;
+        cout << "Посещений в день: ";
+        cin >> v;
+        DME->setVisitsPerDay(v);
+    
+        double avg;
+        cout << "Ср-ее кол-во полётов в день: ";
+        cin >> avg;
+        DME->setAvgNumOfFlightsPerDay(avg);
+    
+        char air;
+        cout << "Класс аэропорта: ";
+        cin >> air;
+        DME->setAirClass(air);
+    
+        bool use;
+        cout << "Состояние: ";
+        cin >> use;
+        DME->setInUseFlag(use);
+    
+    
+    char airCode[SIZE];
+    do
+    {
+        cout << "Код аэропорта: ";
+        cin.ignore();
+        cin.getline(airCode, 3);
+        DME->setAirportCode(airCode);
+    } 
+    while (strcmp(DME->getAirportCode(), airCode) != 0);
+    
+    cout << "Последние полёты: ";
+    
+    for(int i = 0; i < MAX_LAST_FLIGHTS; i++)
+    {
+        int airCode;
+        cout << "Код полёта (под индексом " << i << "): ";
+        cin >> airCode;
+        DME->appendLastFlight(airCode);
+    }
+    
+    Airport *pMSK = &MSK;
+    Airport **ppDME = &DME;
+    
+    Airport &refMSK = MSK;
+    
+    cout << pMSK->СвангаватьКоличествоПолётов(06, 59, 12, 199) << endl;
+    cout << (**ppDME).СвангаватьКоличествоПолётов(06, 59, 12, 199) << endl;
+    cout << refMSK.СвангаватьКоличествоПолётов(06, 59, 12, 199) << endl;
+    
+    delete DME;
     return 0;
 }
